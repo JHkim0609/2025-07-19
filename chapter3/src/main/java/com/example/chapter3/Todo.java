7f70d56e410552b192a37a794da6ef4c9c2720cd
JHkim0609 <no1tdc2@gmail.com>
2025-07-27 22:52:50 +0900

    work

---
+++ chapter3/src/main/java/com/example/chapter3/Todo.java
@@ -0,0 +1,13 @@
+package com.example.chapter3;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Todo {
+    private long id;
+    private String body;
+}
